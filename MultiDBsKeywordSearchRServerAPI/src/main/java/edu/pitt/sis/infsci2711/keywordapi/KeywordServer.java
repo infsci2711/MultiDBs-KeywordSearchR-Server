d1e997114deacd8dfed62d46214781bd15f6b28b
wenzhang61 <wez61@pitt.edu>
2015-04-13 12:28:37 -0400

    sfdg

--- MultiDBsKeywordSearchRServerAPI/src/main/java/edu/pitt/sis/infsci2711/keywordapi/KeywordServer.java
+++ MultiDBsKeywordSearchRServerAPI/src/main/java/edu/pitt/sis/infsci2711/keywordapi/KeywordServer.java
@@ -2,7 +2,7 @@ package edu.pitt.sis.infsci2711.keywordapi;
 
 import edu.pitt.sis.infsci2711.multidbs.utils.JerseyJettyServer;
 
-public class TutorialServer {
+public class KeywordServer {
 	
 	public static void main(final String[] args) throws Exception {
 		JerseyJettyServer server = new JerseyJettyServer(7654, "edu.pitt.sis.infsci2711.keyword.rest");
