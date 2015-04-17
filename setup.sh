2b696abb675d00d461defb89b8b2179dcb202ff6
rachelgg <rachel1208gg@gmail.com>
2015-04-16 20:38:30 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
+#install oracle java 8
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+sudo apt-get install oracle-java8-set-default
+
+#install others
+sudo apt-get install maven
+sudo apt-get install mysql-server
+sudo apt-get install nginx
+
+#folders and environments
+cd /opt
+sudo mkdir project
+cd project
+
+#clone project
+sudo git clone https://github.com/infsci2711/MultiDBs-KeywordSearchR-Server.git
+sudo git clone https://github.com/infsci2711/MultiDBs-Utils.git
+
 mysql -u root -proot < db.sql
