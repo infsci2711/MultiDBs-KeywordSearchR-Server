4232da92da358b975ea7d431811966303b2a453e
rachelgg <rachel1208gg@gmail.com>
2015-04-19 15:17:09 -0400

    Update stop-server.sh

--- stop-server.sh
+++ stop-server.sh
@@ -3,6 +3,7 @@
 #start or (restart) KeywordSearchR
 
 #stop the running server
+cd /opt/project/MultiDBs-KeywordSearchR-Server/MultiDBsKeywordSearchRServerAPI/target
 kill -9 $(ps aux | grep java | grep multidbskeywordsearchrserverapi-0.1-SNAPSHOT.jar | awk '{print $2}')
 
 #install mvn to rebuild java code, prepare for next start
