1101cf3bba551f974915b80df8d048b7bc723f5f
rachelgg <rachel1208gg@gmail.com>
2015-04-19 15:15:56 -0400

    Update start-servers.sh

--- start-servers.sh
+++ start-servers.sh
@@ -3,4 +3,5 @@
 #start or (restart) KeywordSearch
 
 #start our jerseyjetty server
-nohup java -jar MultiDBsKeywordSearchRServerAPI/target/multidbskeywordsearchrserverapi-0.1-SNAPSHOT.jar /opt/project/MultiDBs-KeywordSearchR-Server/MultiDBsKeywordSearchRServerAPI/src/main/resources/config.properties> log.out 2> error.log < /dev/null &
+cd /opt/project/MultiDBs-KeywordSearchR-Server/MultiDBsKeywordSearchRServerAPI/target
+nohup java -jar multidbskeywordsearchrserverapi-0.1-SNAPSHOT.jar /opt/project/MultiDBs-KeywordSearchR-Server/MultiDBsKeywordSearchRServerAPI/src/main/resources/config.properties > log.out 2> error.log < /dev/null &
