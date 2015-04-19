b8786014502e1b4ad111e3bc32bdd2fc397ce6b4
RoxanneC <ruoxuan.cheng@gmail.com>
2015-04-19 14:00:48 -0400

    Update stop-server.sh

--- stop-server.sh
+++ stop-server.sh
@@ -2,5 +2,5 @@
 
 #start or (restart) KeywordSearchR
 
-#start our jerseyjetty server
+#stop the running server
 kill -9 $(ps aux | grep java | grep multidbskeywordsearchrserverapi-0.1-SNAPSHOT.jar | awk '{print $2}')
