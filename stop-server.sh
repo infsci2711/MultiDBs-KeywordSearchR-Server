b2fca745c67c29a7f5566a0c419488c65c93f06d
RoxanneC <ruoxuan.cheng@gmail.com>
2015-04-19 14:11:38 -0400

    Update stop-server.sh

--- stop-server.sh
+++ stop-server.sh
@@ -4,3 +4,11 @@
 
 #stop the running server
 kill -9 $(ps aux | grep java | grep multidbskeywordsearchrserverapi-0.1-SNAPSHOT.jar | awk '{print $2}')
+
+#install mvn to rebuild java code, prepare for next start
+cd /opt/project/MultiDBs-Utils
+mvn install
+cd /opt/project/MultiDBs-KeywordSearchR-Server/MultiDBsKeywordSearchRServer
+mvn install
+cd /opt/project/MultiDBs-KeywordSearchR-Server/MultiDBsKeywordSearchRServerAPI
+mvn install 
