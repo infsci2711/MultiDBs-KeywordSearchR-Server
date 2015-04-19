d169dc6101df9a28bb05ef881afdeb1a8e5832d9
rachelgg <rachel1208gg@gmail.com>
2015-04-19 11:18:36 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-#install oracle java 8
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install oracle-java8-installer
-sudo apt-get install oracle-java8-set-default
+#install 
 
-#install others
 sudo apt-get install maven
+sudo apt-get install git
+sudo apt-get install openjdk-7-jdk
 sudo apt-get install mysql-server
 sudo apt-get install nginx
 
@@ -17,6 +14,22 @@ cd project
 
 #clone project
 sudo git clone https://github.com/infsci2711/MultiDBs-KeywordSearchR-Server.git
+sudo git clone https://github.com/infsci2711/MultiDBs-KeywordSearchR-WebClient.git
 sudo git clone https://github.com/infsci2711/MultiDBs-Utils.git
 
+
 mysql -u root -proot < db.sql
+cd /usr/share/nginx
+sudo rm -R html
+sudo ln -sv /opt/project/MultiDBs-KeywordSearchR-WebClient html
+
+cd /opt/project/MultiDBs-Utils
+mvn install
+
+cd /opt/project/MultiDBs-KeywordSearchR-Server/MultiDBsKeywordSearchRServer
+mvn install
+cd /opt/project/MultiDBs-KeywordSearchR-Server/MultiDBsKeywordSearchRServerAPI
+mvn install 
+
+#kill -9 $(ps aux | grep java | grep multidbskeywordsearchrserverapi-0.1-SNAPSHOT.jar | awk '{print $2}')
+nohup java -jar MultiDBsKeywordSearchRServerAPI/target/multidbskeywordsearchrserverapi-0.1-SNAPSHOT.jar /opt/project/MultiDBs-KeywordSearchR-Server/MultiDBsKeywordSearchRServerAPI/src/main/resources/config.properties> log.out 2> error.log < /dev/null &
