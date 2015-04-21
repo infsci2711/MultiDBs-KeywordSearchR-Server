67589669a279d79879bb362286f923c876297865
wenzhang61 <wez61@pitt.edu>
2015-04-21 10:59:47 -0400

    Update KeywordRestService.java

--- MultiDBsKeywordSearchRServerAPI/src/main/java/edu/pitt/sis/infsci2711/keyword/rest/KeywordRestService.java
+++ MultiDBsKeywordSearchRServerAPI/src/main/java/edu/pitt/sis/infsci2711/keyword/rest/KeywordRestService.java
@@ -101,6 +101,7 @@ public class KeywordRestService {
 		            QueryViewModel QueryViewModel=new QueryViewModel();
 		            
 		            QueryViewModel.setQuery(q);
+		            logger.info("About to run this query on Presto: " + q);
 		            //PUT Request from Jersey Client Example. pass QueryViewModel instance
 		            Response response = JerseyClientUtil.doPut(PropertiesManager.getInstance().getStringProperty("prestostore.rest.base"), PropertiesManager.getInstance().getStringProperty("prestostore.rest.getAllData"), QueryViewModel);
 		            
